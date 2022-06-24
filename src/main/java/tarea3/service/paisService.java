11ef695ff24fc92e8551a638b138cb2e8468855d
CrisPicado <cpicado869@gmail.com>
2022-06-24 09:42:45 -0600

    Commit - Tarea 03 - Clases de CRUD y dependencias en pom, correccion

--- src/main/java/tarea3/service/paisService.java
+++ src/main/java/tarea3/service/paisService.java
@@ -5,10 +5,20 @@
  */
 package tarea3.service;
 
+import java.util.List;
+import tarea3.domain.Pais;
+
 /**
  *
  * @author Cris Picado
  */
 public interface paisService {
+    public List<Pais> getClientes();
+    
+    public void save(Pais pais);//Si el id no esta creado lo crea, si esta creado hace el update
+    
+    public void delete(Pais pais);//Elimina el cliente
+    
+    public Pais getPais(Pais pais); //Obtiene objeto cliente del id que se le indique
     
 }
